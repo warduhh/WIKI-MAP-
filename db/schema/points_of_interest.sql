b91d87d6e2713d548eac77c4fb2f3ae4a88c7190
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-05-01 16:51:50 -0400

    p.o.i table

---
+++ db/schema/points_of_interest.sql
@@ -0,0 +1,12 @@
+CREATE TABLE points_of_interest (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  description TEXT,
+  image_url TEXT,
+  latitude NUMERIC NOT NULL,
+  longitude NUMERIC NOT NULL,
+  map_id INTEGER NOT NULL REFERENCES maps(id),
+  user_id INTEGER NOT NULL REFERENCES users(id),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP
+);
