6ad279570bea82cb331a489995fa46244819d400
eliachow <eliacchow@gmail.com>
2023-05-03 09:12:46 -0700

    insert data into favorite_maps table

---
+++ db/seeds/04_favorite_maps.sql
@@ -0,0 +1,6 @@
+INSERT INTO favorite_maps (map_id, user_id) VALUES (1, 1);
+INSERT INTO favorite_maps (map_id, user_id) VALUES (1, 2);
+INSERT INTO favorite_maps (map_id, user_id) VALUES (2, 1);
+INSERT INTO favorite_maps (map_id, user_id) VALUES (2, 2);
+INSERT INTO favorite_maps (map_id) VALUES (1);
+INSERT INTO favorite_maps (map_id) VALUES (2);
