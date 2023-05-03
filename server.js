f82a6d4080234fc211946fb57681003d0bf66687
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-05-03 02:35:49 -0400

    P.O.I_routes

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const pointsOfInterestRoutes = require('./routes/points_of_interest');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -39,6 +40,7 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
+app.use('/api/points_of_interest', pointsOfInterestRoutes);
 
 // Home page
 // Warning: avoid creating more routes in this file!
