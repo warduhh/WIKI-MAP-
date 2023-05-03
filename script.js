540ecdfc5ec4195320c3dd2423b7e47dd308333c
eliachow <eliacchow@gmail.com>
2023-05-03 07:48:24 -0700

    import api map and format index page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let location = { lat: -34.397, lng: 150.644 }
 
 async function initMap() {
   //@ts-ignore
-  const { Map } = await google.maps.Map;
+  const { Map } = await google.maps.importLibrary("maps");
 
   map = new Map(document.getElementById("map"), {
     center: location,
