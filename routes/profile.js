4ed294ffdb3481fa4548413d9209cfa2e644ee06
eliachow <eliacchow@gmail.com>
2023-05-04 08:09:40 -0700

    refactor fav_maps by mapID route to render list on the profile page.

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const { getFavMapsByUser } = require('../db/queries/favorite_maps')
+const { getFavMapsByUser, getFavMapsByMapID } = require('../db/queries/favorite_maps')
 
 // 1. Read favorite maps (authenticated user) -- Requirement: a user can view their favorite maps in their profile. 
   router.get('/:id', (req, res) => { 
@@ -35,6 +35,24 @@ const { getFavMapsByUser } = require('../db/queries/favorite_maps')
         console.log("error: ", err);
      })
 }); 
+
+// 2. Browse favorite_maps by map_id (any user) -- Requirement: users can favorite a map
+router.get('/', (req, res) => {
+  const mapID = req.query.mapID
+  getFavMapsByMapID(mapID)
+  .then(data => {
+    console.log("data.rows: ", data.rows)
+    res.render('profile', { favMaps: data.rows })
+  })
+  .catch(err => {
+    res
+        .status(500)
+        .render('error', {
+          message: 'Something went wrong'
+        });
+        console.log("error: ", err);
+  })
+});
 
 
 module.exports = router;
